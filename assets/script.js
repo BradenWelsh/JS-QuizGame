51ae655cf8f181ba25e9c6905f9bdffc0c7beaa2
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-03-14 18:54:43 -0500

    Added timer, buttons, start screen, questions, question checker, and end screen to JS

---
+++ assets/script.js
@@ -0,0 +1,28 @@
+//timer
+var timer = document.querySelector("#timer");
+var timeLeft = 60;
+
+//buttons
+var startButton = document.querySelector("#startButton");
+var viewHiS = document.querySelector("#viewHis")
+var btnA = document.querySelector("#a");
+var btnB = document.querySelector("#b");
+var btnC = document.querySelector("#c");
+var btnD = document.querySelector("#d");
+var submitB = document.querySelector("#submit");
+var clearB = document.querySelector("#clear");
+var retryB = document.querySelector("#retry");
+
+//Start Screen
+
+var startScreen = document.querySelector("#startScreen");
+
+// Questions 
+var questionSect = document.querySelector("#questionSect");
+
+// Question Checker
+var check = document.querySelector("#check");
+
+// Scores
+var Score = document.querySelector("#endScore");
+var initials = document.querySelector("#input");
