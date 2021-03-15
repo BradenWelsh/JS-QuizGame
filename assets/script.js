b5ed42bf70e33829e584cf7380b21d3e4e2cb73f
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-03-14 19:22:30 -0500

    Finished the questions section in JS

--- assets/script.js
+++ assets/script.js
@@ -23,6 +23,67 @@ var questionSect = document.querySelector("#questionSect");
 // Question Checker
 var check = document.querySelector("#check");
 
-// Scores
+// End Screen
 var Score = document.querySelector("#endScore");
 var initials = document.querySelector("#input");
+var endScreen = document.querySelector("#endScreen")
+
+var highScores = [];
+var allHighScores = [];
+// Questions
+var questions = [
+{
+    question: "Inside which HTML element do we put the Java Script?",
+    answers: ["A. <js>",
+            "B. <scripting>",
+            "C. <javascript>",
+            "D. <script>"],
+    correctAnswer: "D. <script>";
+},
+{
+    question: "What is the correct syntax for referring to an external script called 'xxx.js'?",
+    answers: ["A. <script href='xxx.js'>",
+            "B. <script src= ' xxx.js'>",
+            "C. <script name = 'xxx.js'>",
+            "D. <script = 'xxx.js'>"],
+    correctAnswer: "B. <script src= ' xxx.js'>";
+},
+{
+    question: "How do you create a function in JavaScript?",
+    answers: ["A. function myFunction()",
+            "B. function:myFunction()",
+            "C. function = myFunction()",
+            "D. function:'myFunction()"],
+    correctAnswer: "A. function myFunction()";
+
+},
+{
+    question: "How to write an IF statement in JavaScript?",
+    answers: ["A. if i == 5 then",
+            "B. if i = 5 then,"
+            "C. if(i==5)",
+            "D. if i =5"],
+    correctAnswer: "C. if(i==5)";
+}
+];
+function start(){
+    startScreen.style.display = "none";
+    questionSect.style.display = "block";
+}
+startButton.addEventListener("click", start)
+
+submitB.addEventListener("click", function(){
+
+})
+
+retryB.addEventListener("click", function(){
+
+})
+
+viewHiS.addEventListener("click", function(){
+
+})
+
+clearB.addEventListener("click", function(){
+
+})
